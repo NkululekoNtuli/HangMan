abc33cca63137066bfc082bd6099e4d5bcbf8ce3
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-09-16 20:09:54 +0200

    unfinished

--- hangman_attempt.py
+++ hangman_attempt.py
@@ -12,15 +12,20 @@ word_len = len(secret_word)
 print("The word has ", word_len,"letters")
 
 attempt = 0
+origin_word = secret_word_caps
 while attempt < 6:
     guess = input("Guess a letter: ")
     guess_caps = guess.upper()
+#For when a guess is correct
     if guess_caps in secret_word_caps:
         print("Correct! \nThere is", secret_word.count(guess), guess_caps,"in the word" )
         secret_word_caps = secret_word_caps.replace(guess_caps,"")
         if len(secret_word_caps) == 0:
             print("You win!!")
+            print("The word is",origin_word)
+            break #To exit out of the loop because the game has been won
         attempt -= 1
+#For when a guess is incorrect
     elif attempt == 0:
         print("Incorrect!")
         head, torso, left_arm, right_arm = body_parts()
@@ -48,3 +53,4 @@ while attempt < 6:
         print("     ",head,"\n   ",left_arm,torso,right_arm,"\n    ",left_arm,right_arm)
         #Uses right arm because its the same as the right leg
     attempt += 1
+    
